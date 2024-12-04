8506a10b2939b0387ee4f7b10d124ceadb9bed60
rabbi-mehedi <rabbyrithik@gmail.com>
2024-12-04 14:57:57 +0600

    Add Icon and Logo to Public Storage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bangladesh Angels Network Limited</title>
+    <link rel="icon" type="image/webp" href="{{asset('icon.webp')}}">
 </head>
 <body>
     
