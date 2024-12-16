383c51b790361f1a883db5b1925211a229dcea9a
rabbi-mehedi <rabbyrithik@gmail.com>
2024-12-16 11:51:16 +0600

    Turn Portfolio Company as a Reusable Component

---
+++ app/Livewire/PortfolioCompany.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PortfolioCompany extends Component
+{
+    public function render()
+    {
+        return view('livewire.portfolio-company');
+    }
+}
