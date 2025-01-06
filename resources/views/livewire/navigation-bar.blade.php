5e4910f7ce5110a55955653b3354059dae2fdf13
rabbi-mehedi <rabbyrithik@gmail.com>
2025-01-06 20:41:32 +0600

    Update navigation-bar.blade.php

--- resources/views/livewire/navigation-bar.blade.php
+++ resources/views/livewire/navigation-bar.blade.php
@@ -30,6 +30,11 @@
                         <li class="px-4 py-2 hover:bg-gray-100">
                             <a href="{{ route('profile.edit') }}">Profile</a>
                         </li>
+                        @if (auth()->user()->isAdmin())
+                            <li class="px-4 py-2 hover:bg-gray-100">
+                                <a href="{{ route('dashboard') }}">Admin Panel</a>
+                            </li>
+                        @endif
                         <li class="px-4 py-2 hover:bg-gray-100">
                             <a href="{{ route('dashboard') }}">Dashboard</a>
                         </li>
