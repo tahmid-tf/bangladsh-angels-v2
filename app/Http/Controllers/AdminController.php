95696e061cd940ac7a638acc1dd809c7bd0ea259
rabbi-mehedi <rabbyrithik@gmail.com>
2025-01-12 16:36:12 +0600

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -130,7 +130,7 @@ class AdminController extends Controller
             'designation' => 'nullable|string|max:255',
             'joining_date' => 'nullable|date',
             'renewed' => 'nullable|string|max:255',
-            'country' => 'required|string|max:100',
+            'primary_country' => 'required|string|max:100',
             'preference_sector' => 'nullable|string|max:255',
             'strategic_analyst' => 'nullable|in:TL,FS,TB',
             'photo' => 'nullable|image|max:3072', // Max size: 3MB
@@ -158,7 +158,7 @@ class AdminController extends Controller
             'designation' => $request->designation,
             'joining_date' => $request->joining_date,
             'renewed' => $request->renewed,
-            'country' => $request->country,
+            'primary_country' => $request->country,
             'preference_sector' => $request->preference_sector,
             'strategic_analyst' => $request->strategic_analyst,
             'role' => 'investor', // Assign the investor role
