1010677a756992298ab1bf53ee3f83a9209d99d4
rabbi-mehedi <rabbyrithik@gmail.com>
2025-01-16 00:20:24 +0600

    Update index.blade.php

--- resources/views/deals/index.blade.php
+++ resources/views/deals/index.blade.php
@@ -72,7 +72,7 @@
                 <input type="hidden" name="deal_id" value="{{ $deal->id }}">
                 <input type="hidden" name="user_id" value="{{ auth()->id() }}">
                 @if ($deal->type!=="invest")
-                    <a href="{{$deal->action_link}}" class="w-full p-3 mt-4 bg-[#36b37e] text-white py-2 rounded-full font-semibold hover:bg-green-600 transition">
+                    <a href="{{$deal->action_link}}" class="w-full p-3 mt-6 bg-[#36b37e] text-white py-2 rounded-full font-semibold hover:bg-green-600 transition">
                         {{ucfirst($deal->type)}}
                     </a>    
                 @else
