9d81bef3ffea06423de40916f232a42a6225a4d0
rabbi-mehedi <rabbyrithik@gmail.com>
2025-01-19 10:39:14 +0600

    Update edit.blade.php

--- resources/views/admin/members/edit.blade.php
+++ resources/views/admin/members/edit.blade.php
@@ -71,6 +71,8 @@
             <div>
                 <label for="country" class="block font-semibold text-gray-700">Country</label>
                 <select id="country" name="primary_country" required class="border border-gray-300 p-2 rounded w-full">
+                    
+                    <option disabled value="{{$user->primary_country}}">{{ $user->primary_country }}</option>
                     <option value="Afghanistan">Afghanistan</option>
                     <option value="Åland Islands">Åland Islands</option>
                     <option value="Albania">Albania</option>
