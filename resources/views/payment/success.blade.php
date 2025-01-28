9f74fbb449b22487f6f7f30b10ac6e2eda79e308
rabbi-mehedi <rabbyrithik@gmail.com>
2025-01-28 23:09:39 +0600

    Update success.blade.php

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -5,6 +5,43 @@
     <h1 class="text-3xl font-bold text-gray-800 mb-4">Thank You for Your Purchase!</h1>
     <p class="text-gray-500">We’ve sent an email to your provided email address with the payment details.</p>
     <p class="text-gray-500">Feel free to contact us if you have any questions.</p>
+    <table>
+        <th>Analyst</th>
+        <th>Contact</th>
+        <tr>
+            <td>
+                Mohaimenul Islam
+            </td>
+            <td>
+                <a href="tel:+8801731814993">+8801731814993</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Mustavi Khan
+            </td>
+            <td>
+                <a href="tel:+8801727612346">+8801727612346</a>
+                
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Farin Sabrina
+            </td>
+            <td>
+                <a href="tel:+8801705181801">+8801705181801</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Rifat Ara Bohny
+            </td>
+            <td>
+                <a href="tel:+8801687387288">+8801687387288</a> 
+            </td>
+        </tr>
+    </table>
     <a href="{{ route('home') }}" class="mt-6 inline-block px-6 py-2 bg-green-600 text-white rounded-full hover:bg-green-700 transition">Back to Home</a>
 </div>
 @endsection
