f08499aae7b092bcbc0a90c940f582444fb491a2
rabbi-mehedi <rabbyrithik@gmail.com>
2025-01-30 21:13:11 +0600

    Update portfolio-company.blade.php

--- resources/views/livewire/portfolio-company.blade.php
+++ resources/views/livewire/portfolio-company.blade.php
@@ -1,15 +1,18 @@
 <div class="w-[1/3] m-6">
     <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
+      <a href="{{route('deal.view',$deal->id)}}">
         <img
           alt=""
           src="{{$deal->getCoverUrl()}}"
           class="h-56 w-full object-cover"
         />
+      </a>
+        
       
         <div class="bg-white p-4 sm:p-6">
           <time datetime="2022-10-10" class="block text-xs text-gray-500"> {{$deal->created_at->diffForHumans()}} </time>
       
-          <a href="#">
+          <a href="{{route('deal.view',$deal->id)}}">
             <h3 class="mt-0.5 text-lg text-gray-900">{{$deal->title}}</h3>
           </a>
       
