878919561bd98a3ade6abfbca17ef553443c70c4
rabbi-mehedi <rabbyrithik@gmail.com>
2025-02-02 23:37:56 +0600

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,9 +1,6 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
-
-    <!-- Session Status -->
+@extends('layouts.guest')
+@section('page_title','Upgrade Plan | Bangladesh Angels Network')
+@section('page_content')
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('password.email') }}">
@@ -22,4 +19,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+@endsection
