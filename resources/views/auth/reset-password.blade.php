2d7e2099621e8e2198d2872b56b836c1b0b3c890
rabbi-mehedi <rabbyrithik@gmail.com>
2025-02-03 00:05:40 +0600

    Update reset-password.blade.php

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,6 @@
-<x-guest-layout>
+@extends('layouts.guest')
+@section('page_title','Forgot Password | Bangladesh Angels Network')
+@section('page_content')
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -36,4 +38,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+@endsection
