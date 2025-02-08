92f25e768bd83daafb6a9f35788f9e0c828e8c17
rabbi-mehedi <rabbyrithik@gmail.com>
2025-02-08 12:07:46 +0600

    Update navigation-bar.blade.php

--- resources/views/livewire/navigation-bar.blade.php
+++ resources/views/livewire/navigation-bar.blade.php
@@ -15,9 +15,9 @@
         <div class="hidden md:flex flex-1 justify-center">
             <ul class="flex flex-row font-bold text-gray-700">
                 <li class="m-3"><a href="{{ route('deals') }}">Deals</a></li>
-                <li class="m-3"><a href="{{route('investors')}}">BAN Investors</a></li>
+                <li class="m-3"><a href="{{route('investors')}}">Investors</a></li>
                 <li class="m-3"><a href="{{route('portfolio')}}">Portfolio</a></li>
-                <li class="m-3"><a href="{{route('resources')}}">BAN Resources</a></li>
+                <li class="m-3"><a href="{{route('resources')}}">Resources</a></li>
                 <li class="m-3"><a href="{{ route('team') }}">Our Team</a></li>
             </ul>
         </div>
