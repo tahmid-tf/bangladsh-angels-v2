0f18638346f192ae940214178fbfc17a1d84cf2e
rabbi-mehedi <rabbyrithik@gmail.com>
2025-02-11 20:51:11 +0600

    Style Update to Mail Template

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -34,7 +34,7 @@
 <table role="presentation" width="100%" cellspacing="0" cellpadding="0" style="margin-top: 30px; padding: 20px; text-align: center; background-color: #f9f9f9; border-radius: 5px;">
     <tr>
         <td align="center" style="color: #333; font-size: 12px;">
-            © {{ date('Y') }} Bangladesh Angels, all rights reserved <br>
+            © {{ date('Y') }} Bangladesh Angels, <a href="https://www.linkedin.com/company/bangladesh-angels/" style="color: #16a34a; text-decoration: none;">LinkedIn</a> | <a href="https://bdangels.co" style="color: #16a34a; text-decoration: none;">Our Website</a><br>
             Telephone: +8801823998877 | <a href="mailto:hello@bdangels.co" style="color: #16a34a; text-decoration: none;">hello@bdangels.co</a>
         </td>
     </tr>
