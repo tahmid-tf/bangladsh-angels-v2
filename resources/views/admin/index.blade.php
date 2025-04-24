d2f59b4aa162d406a0d279e1454afd3c66ac0eef
rabbi-mehedi <rabbyrithik@gmail.com>
2025-04-25 00:55:58 +0600

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -52,7 +52,7 @@
     
     <div class="p-5 bg-white rounded-lg shadow hover:shadow-lg transition-shadow">
       <div class="flex justify-between items-center mb-2">
-        <h2 class="text-2xl font-bold text-gray-800">{{ number_format($totalRevenue, 2) }}</h2>
+        <h2 class="text-2xl font-bold text-gray-800">BDT {{ number_format($totalRevenue, 2) }}</h2>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-amber-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
