f89638de0aa490703ff2ab678a5f19abcdc791e9
rabbi-mehedi <rabbyrithik@gmail.com>
2025-05-04 23:18:59 +0600

    ok

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -1,9 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Payment Successful') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
+@section('content')
 
     <div class="py-12">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
@@ -56,4 +52,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
