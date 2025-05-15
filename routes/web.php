365d14eeaf0282fa200cccfdef3e5d79a1817a8d
rabbi-mehedi <rabbyrithik@gmail.com>
2025-05-15 22:40:48 +0600

    Revert "Update web.php"
    This reverts commit c887bddda51a43daf4a541a326ad7bc31a00cbc5.

--- routes/web.php
+++ routes/web.php
@@ -19,24 +19,8 @@ use App\Models\Payment;
  * Public Routes
  */
 Route::get('/', PrimaryController::class)->name('home');
-Route::get('/investors', function () {
-    if (!Auth::check()) {
-        return redirect()->route('upgrade.page');
-    }
-    if (auth()->user()->account_status == "free") {
-        return redirect()->route('upgrade.page');
-    }
-    return app(PrimaryController::class)->viewInvestors();
-})->middleware(['verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('investors');
-Route::get('/resources', function () {
-    if (!Auth::check()) {
-        return redirect()->route('upgrade.page');
-    }
-    if (auth()->user()->account_status == "free") {
-        return redirect()->route('upgrade.page');
-    }
-    return app(PrimaryController::class)->viewResources();
-})->middleware(['verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('resources');
+Route::get('/ban-investors',[PrimaryController::class,'viewInvestors'])->middleware(['auth', 'verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('investors');
+Route::get('/ban-resources',[PrimaryController::class,'viewResources'])->middleware(['auth', 'verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('resources');
 Route::get('/portfolio',[PrimaryController::class,'viewPortfolio'])->name('portfolio');
 Route::get('/approval/pending', function() {
     return view('approval.pending');
@@ -56,44 +40,32 @@ Route::prefix('upgrade')->group(function () {
 });
 
 Route::get('/deals', function () {
-    if (!Auth::check()) {
-        return redirect()->route('upgrade.page');
-    }
-    if (auth()->user()->account_status == "free") {
+    if (!Auth::check() || auth()->user()->account_status == "free") {
         return redirect()->route('upgrade.page');
     }
     return app(PrimaryController::class)->viewDeals();
-})->middleware(['verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('deals');
+})->middleware(['auth', 'verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('deals');
 
 Route::get('/deals/invest', function () {
-    if (!Auth::check()) {
-        return redirect()->route('upgrade.page');
-    }
-    if (auth()->user()->account_status == "free") {
+    if (!Auth::check() || auth()->user()->account_status == "free") {
         return redirect()->route('upgrade.page');
     }
     return app(PrimaryController::class)->viewDeals_invest();
-})->middleware(['verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('deals.invest');
+})->middleware(['auth', 'verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('deals.invest');
 
 Route::get('/deals/commit', function () {
-    if (!Auth::check()) {
-        return redirect()->route('upgrade.page');
-    }
-    if (auth()->user()->account_status == "free") {
+    if (!Auth::check() || auth()->user()->account_status == "free") {
         return redirect()->route('upgrade.page');
     }
     return app(PrimaryController::class)->viewDeals_commit();
-})->middleware(['verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('deals.commit');
+})->middleware(['auth', 'verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('deals.commit');
 
 Route::get('/deals/review', function () {
-    if (!Auth::check()) {
-        return redirect()->route('upgrade.page');
-    }
-    if (auth()->user()->account_status == "free") {
+    if (!Auth::check() || auth()->user()->account_status == "free") {
         return redirect()->route('upgrade.page');
     }
     return app(PrimaryController::class)->viewDeals_review();
-})->middleware(['verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('deals.review');
+})->middleware(['auth', 'verified', \App\Http\Middleware\ApprovedUserMiddleware::class])->name('deals.review');
 
 Route::get('/faq', [PrimaryController::class, 'viewFAQ'])->name('faq');
 Route::get('/our-team', [PrimaryController::class, 'viewTeam'])->name('team');
